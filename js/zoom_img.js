02d67dad2853ce2d96796add740d41c8d6e05dea
YevhDr <evgesha.drozdova@gmail.com>
2022-02-16 22:03:43 +0200

    укр танки

--- js/zoom_img.js
+++ js/zoom_img.js
@@ -1,53 +1,59 @@
-       const origin = document.getElementById("zoom-pic");
+const url_ru = 'https://texty.org.ua/media/images/scheme2_HYXdH2f.original.jpg';
+const url_ua = 'https://texty.org.ua/media/images/scheme_ukr.original.jpg'
        
-       var width = origin.offsetWidth;
-       var height = origin.offsetHeight;
-       const ratio = width / height;
+const origin_ru = document.getElementById("rus-tanks");
+const origin_ua = document.getElementById("ukr-tanks");
 
-       
+drawCanvas(origin_ru, "canvas#rus-tanks-canvas", url_ru);
 
-        var canvas = d3.select("canvas")
-            .attr("width", width)
-            .attr("height", height); 
+drawCanvas(origin_ua, "canvas#ukr-tanks-canvas", url_ua);
+
+
+function drawCanvas(origin, container, url){
+    const width = origin.offsetWidth;
+    const height = origin.offsetHeight;
+    const ratio = width / height;
+
+    const canvas = d3.select(container)
+    .attr("width", width)
+    .attr("height", height); 
+
+
+    var context = canvas.node().getContext("2d");   
+    var image = new Image();
             
+    image.onload = function () {
+        context.drawImage(image, 0, 0, width, height);  
+        origin.remove();
+    };
+    image.src = url;
 
-        var context = canvas.node().getContext("2d");        
-        var image = new Image();
-        var url = 'https://texty.org.ua/media/images/scheme2_cNttWen.original.jpg';
-
-        image.onload = function () {
-            context.drawImage(image, 0, 0, width, height);  
-            origin.remove();
-        };
-
-        image.src = url;
-
-        /* додаємо зум */
-        canvas.call(d3.zoom()
-            .scaleExtent([1, 5])
-            .on("zoom", zoom)
-            .filter(() => !d3.event.button && d3.event.ctrlKey)
-            )
-            .on("wheel", () => { if (d3.event.ctrlKey) d3.event.preventDefault() });
+    canvas.call(d3.zoom()
+        .scaleExtent([1, 5])
+        .on("zoom", zoom)
+        .filter(() => !d3.event.button && d3.event.ctrlKey)
+        )
+        .on("wheel", () => { if (d3.event.ctrlKey) d3.event.preventDefault() });
 
 
-        function zoom() {           
-                var transform = d3.event.transform;
-                context.save();
-                context.clearRect(0, 0, width, height);
-                context.translate(transform.x, transform.y);
-                context.scale(transform.k, transform.k);
-                context.drawImage(image, 0, 0, width, height);
-                context.restore();              
-        }
+    function zoom() {           
+            var transform = d3.event.transform;
+            context.save();
+            context.clearRect(0, 0, width, height);
+            context.translate(transform.x, transform.y);
+            context.scale(transform.k, transform.k);
+            context.drawImage(image, 0, 0, width, height);
+            context.restore();              
+    }
 
     window.addEventListener("resize", function(){
-        d3.select("canvas")
-            .attr("width", window.innerWidth)
-            .attr("height", window.innerWidth / ratio);    
 
+        d3.select(container)
+            .attr("width", window.innerWidth)
+            .attr("height", window.innerWidth / ratio);
 
         context.clearRect(0, 0, width, height);
         context.drawImage(image, 0, 0,  window.innerWidth, window.innerWidth / ratio);
         context.restore();             
     });
+}
